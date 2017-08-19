03a0cb7074e8269c357446d76677e1192a94b75f
Szymon Nieradka <szymon[]nieradka.net>
2017-08-19 16:51:28 +0200

    second excersise

---
+++ e2/README.md
@@ -0,0 +1,17 @@
+# Differences between arrays
+
+Write a JavaScript function to find the difference of two arrays.
+
+Examples:
+
+`console.log(difference([1, 2, 3], [100, 2, 1, 10]));`
+`["3", "10", "100"]`
+
+`console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));`
+`["6"]`
+
+`console.log(difference([1, 2, 3], [100, 2, 1, 10]));`
+`["3", "10", "100"]`
+
+[source](http://www.w3resource.com/javascript-exercises/javascript-array-exercises.php)
+
---
+++ e2/e2.js
@@ -0,0 +1,27 @@
+function difference(arr1, arr2){
+	return [3, 10, 100];
+}
+
+Array.prototype.compare = function(array) {
+	if (!array) {
+		return false;
+	}
+	if (this.length !== array.length) {
+		return false;
+	}
+	for (var i = 0, l = this.length; i < l; i++) {
+		if (this[i] instanceof Array && array[i] instanceof Array) {
+			if (!this[i].compare(array[i])) {
+				return false;
+			}
+		}
+		else if (this[i] !== array[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.assert(difference([1, 2, 3], [100, 2, 1, 10]).compare(["3", "10", "100"]), 'test1');
+console.assert(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]).compare(["6"]), 'test2');
+console.assert(difference([1, 2, 3], [100, 2, 1, 10]).compare(["3", "10", "100"]), 'test3');
